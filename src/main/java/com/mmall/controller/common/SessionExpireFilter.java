7b50388094e607355b436bb9044b25d541f5bab0
zuo <844307326@qq.com>
2019-09-01 20:59:50 +0800

    commit

--- src/main/java/com/mmall/controller/common/SessionExpireFilter.java
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -34,7 +34,7 @@ public class SessionExpireFilter implements Filter {
             //判断logintoken是否为空或者""；
             //如果不为空的话，符合条件，继续拿user信息
 
-            String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+            String userJsonStr = RedisPoolUtil.get(loginToken);
             User user = JsonUtil.string2Obj(userJsonStr,User.class);
             if(user != null){
                 //如果user不为空，则重置session的时间，即调用expire命令
